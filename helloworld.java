307b7514e215d0c75ccf71ba39a9792cc2d49b1f
AlenaSazykina <443as.learn.ac@gmail.com>
2025-06-03 21:15:48 +0300

    output twice hello world

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,8 @@
 public class Main{
     public static void main(String[]arg){
-        System.out.println("Hello world");
+        int i = 0;
+        while (i < 2) {
+            System.out.println("Hello, world!");
+            i++;
     }
 }
